88c0bfc66729f4d6c56b584d8d802ec3318bb393
LUIZHVA <luizhva02@gmail.com>
2023-09-06 12:26:28 -0300

    06/09/2023

--- intellij_projects/biblioteca/src/br/senai/sp/jandira/biblioteca/model/Livros.java
+++ intellij_projects/biblioteca/src/br/senai/sp/jandira/biblioteca/model/Livros.java
@@ -1,6 +1,20 @@
 package br.senai.sp.jandira.biblioteca.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Livros {
+    Random random = new Random();
     public void MethodLivros() {
+        List<Livros> LivrosCadastrados = new ArrayList<>();
+        LivrosCadastrados.add(Livros);
+
+    }
+    public void CadastrarLivros(){
+
+        for(int n=1;Livros LivrosCadastrados : Livros;n++){
+
+        }
     }
 }
