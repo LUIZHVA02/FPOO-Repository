063c8e33709ed06b8942740991fd2d7ac7eedf26
LUIZHVA <luizhva02@gmail.com>
2023-11-01 12:27:41 -0300

    01-11-2023-12:11

--- intellij_projects/gerenciador_de_voos/src/br/senai/sp/jandira/Main.java
+++ intellij_projects/gerenciador_de_voos/src/br/senai/sp/jandira/Main.java
@@ -5,8 +5,11 @@ import br.senai.sp.jandira.model.Gol;
 import br.senai.sp.jandira.model.Latam;
 import br.senai.sp.jandira.model.Voos;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
         Voos azulVoo1 = new Azul();
         Voos azulVoo2 = new Azul();
@@ -20,5 +23,23 @@ public class Main {
         Voos latamVoo2 = new Latam();
         Voos latamVoo3 = new Latam();
 
+        boolean exit = false;
+
+        while (!exit){
+            System.out.println("          Bem-Vindo          ");
+            System.out.println("1 - Consultar Voos           ");
+            System.out.println("2 - Sair                     ");
+            int escolha = scanner.nextInt();
+
+            switch (escolha){
+                case 1:
+                    
+                    break;
+
+                case 2:
+                    exit = true;
+                    break;
+            }
+        }
     }
 }
